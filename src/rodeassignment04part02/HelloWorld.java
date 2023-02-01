b847acd8933bda5b583ffddfa9b14e6924a03d76
michael_rode <michael_rode@apple.com>
2023-01-31 20:51:45 -0800

    added main method that prints hello world

--- src/rodeassignment04part02/HelloWorld.java
+++ src/rodeassignment04part02/HelloWorld.java
@@ -1,4 +1,11 @@
 package rodeassignment04part02;
 
+/**
+ * Created by Mike Rode on Jan 31, 2023
+ */
+
 public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
 }
